12c6f136693b381182886fd90e10f6c1c886071d
An Do Eon <andoune0719@kookmin.ac.kr>
2018-10-29 00:48:07 +0900

    Create Baekjoon3053.java

---
+++ 4 week/Baekjoon3053.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String r1 = String.format("%.6f", Math.PI * a * a);
+        String r2 = String.format("%.6f", (double)2 * a * a);
+        
+        System.out.println(r1);
+        System.out.println(r1);
+    }
+}
