564a21c6f538d5f637cadfba05599173f1bcf65b
An Do Eon <andoune0719@kookmin.ac.kr>
2018-10-29 01:06:54 +0900

    Create Beakjoon3026.java

---
+++ 4 week/Beakjoon3026.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,r;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        r = b*2 - a;
+        System.out.println(r);
+    }
+}
+     
